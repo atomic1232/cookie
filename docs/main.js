6ce99c75140403ed39433df0a7946a9a1820c6b3
Atomic <152748418+atomic12321@users.noreply.github.com>
2025-03-05 10:53:18 -0800

    Added KlattmoseUtilities.js

--- docs/main.js
+++ docs/main.js
@@ -16892,6 +16892,7 @@ setTimeout(() => {
     Game.LoadMod('mods/fortunecookie.js'); // Ensure correct filename and extension
 }, 2000); // 2-second delay
 Game.LoadMod('https://staticvariablejames.github.io/InsugarTrading/InsugarTrading.js');
+ Game.LoadMod('mods/KlattmoseUtilities.js');
 
 // Load Cookie Monster after Fortune Cookie with another delay
 setTimeout(() => {
